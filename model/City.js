0b5912811239b10a2a3ce2a38cb23be3353ea575
Mohammad <m7md389@gmail.com>
2021-12-01 11:56:46 +0200

    ..

--- model/City.js
+++ model/City.js
@@ -1,6 +1,6 @@
 import Mongoose from "mongoose"
 const Schema = Mongoose.Schema
-Mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/weatherapp")
+    // Mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/weatherapp")
 
 const citySchema = new Schema({
     name: String,
