bbfcb760a12d790ca2eedd5ba652d529f6ddc50c
Mohammad <m7md389@gmail.com>
2021-12-01 17:31:18 +0200

    renderer done

--- dist/render.js
+++ dist/render.js
@@ -1,9 +1,13 @@
 class Renderer {
     constructor() {
-
+        const citiesEl = $('.cities-container')
+        const HandlebarsSource = $("#city-template").html()
+        const template = Handlebars.compile(HandlebarsSource)
     }
 
-    renderData(allCityData) {
-
+    renderData(citiesData) {
+        citiesEl.empty()
+        const newHTML = template({ city: citiesData })
+        citiesEl.append(newHTML)    
     }
 }
