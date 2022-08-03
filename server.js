b3435bc0f9a590dcdf4d7967bfb4d74b43688f04
Mohammad <m7md389@gmail.com>
2022-08-03 10:03:44 +0300

    port fix

--- server.js
+++ server.js
@@ -2,7 +2,9 @@ import express, { json, urlencoded } from "express";
 import { join, resolve } from "path";
 import api from "./server/routes/api.js";
 import Mongoose from "mongoose";
-Mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/weatherapp", {
+
+const MONGODB_URI = process.env.MONGODB_URI ?? "mongodb://localhost/weatherapp";
+Mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true
 });
 
@@ -16,7 +18,7 @@ app.use(express.static(join(__dirname, "node_modules")));
 
 app.use("/", api);
 
-const PORT = 3000;
-app.listen(process.env.PORT || PORT, function () {
+const PORT = process.env.PORT ?? 3000;
+app.listen(PORT, function () {
   console.log(`Weather App server is running on port ${PORT}`);
 });
